224c78ca06cd5ae326e8852e43e082154903a3c8
Jaouher1 <Hammoudajaouhar@gmail.com>
2020-01-02 18:09:40 +0100

    latest

--- Identity.JwtToken/Extensions/Extensions.cs
+++ Identity.JwtToken/Extensions/Extensions.cs
@@ -7,37 +7,31 @@ using System.Text;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
+using Identity.JwtToken.Models;
 
 namespace Identity.JwtToken.Extensions
 {
-   public static class Extensions
+    public static class Extensions
     {
-
-        public static string GenerateJwtToken(this IdentityUser user , IConfiguration configuration , TokenOptions options = null)
+        public static string GenerateJwtToken(this IdentityUser user, IConfiguration configuration,
+            Models.TokenOptions options = null)
         {
-            var claims = new[] {
+            var claims = new[]
+            {
                 new Claim(JwtRegisteredClaimNames.Sub, user.Id),
                 new Claim(JwtRegisteredClaimNames.Email, user.Email),
                 new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
             };
-    
-                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration[options?.ConfKeyPath ?? "Jwt:Key"]));
-                var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-                var token = new JwtSecurityToken(configuration[options?.ConfIssuerPath ?? "Jwt:Issuer"],
-                    configuration[options?.ConfAudiencePath ?? "Jwt:Audience"],
-                    claims,
-                    expires: DateTime.Now.AddMinutes(options?.ValidTimeInMinute ?? 30),
-                    signingCredentials: credentials);
-                return new JwtSecurityTokenHandler().WriteToken(token);
 
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(configuration[options?.ConfKeyPath ?? "Jwt:Key"]));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(configuration[options?.ConfIssuerPath ?? "Jwt:Issuer"],
+                configuration[options?.ConfAudiencePath ?? "Jwt:Audience"],
+                claims,
+                expires: DateTime.Now.AddMinutes(options?.ValidTimeInMinute ?? 30),
+                signingCredentials: credentials);
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
-
-   public class TokenOptions
-   {
-       public long ValidTimeInMinute { get; set; }
-       public string ConfKeyPath { get; set; }
-       public string ConfIssuerPath { get; set; }
-       public string ConfAudiencePath { get; set; }
-   }
 }
---
+++ Identity.JwtToken/Models/TokenOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Identity.JwtToken.Models
+{
+    public class TokenOptions
+    {
+        public long ValidTimeInMinute { get; set; }
+        public string ConfKeyPath { get; set; }
+        public string ConfIssuerPath { get; set; }
+        public string ConfAudiencePath { get; set; }
+    }
+}
